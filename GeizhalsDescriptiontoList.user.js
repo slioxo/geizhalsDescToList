be3240d3f75f3dcfa1196811d00b1425264fbd52
slioxo <lioxo_go@yahoo.de>
2017-02-24 18:33:11 +0100

    added comments and universality
    now also works on geizhals.eu and geizhals.at

--- GeizhalsDescriptiontoList.user.js
+++ GeizhalsDescriptiontoList.user.js
@@ -4,18 +4,24 @@
 // @version      0.1
 // @description  Makes the description of Geizhals articles a list rather than a text
 // @author       http://t.me/lioxo
-// @match        http://geizhals.de/*
+// @include      http://geizhals.de/*
+// @include      http://geizhals.eu/*
+// @include      http://geizhals.at/*
 // @grant        none
 // ==/UserScript==
 
+//get div with product description
 var prodDesc = document.getElementById('gh_proddesc');
 if(prodDesc){
+    //extract innerHTML, split by dot symbols, and create the list from the resulting array
+    //then, insert this HTML into the div
     var prodDescText = prodDesc.innerHTML;
     var prodDescArray = prodDescText.split(" • ");
     var prodDescList = createList(prodDescArray);
     prodDesc.innerHTML = prodDescList;
 }
 
+//create a simple unordered list from a given array
 function createList(listArray){
     var html = '<ul>';
     listArray.forEach(function appendAsListPoint(listItem){
